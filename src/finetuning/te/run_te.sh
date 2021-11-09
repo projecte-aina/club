fff1d4d8683e5241c97896bcf6c7e2ba6ce63f72
Casimiro Pio Carrino <c.casimiro88@gmail.com>
2021-11-09 10:27:16 +0100

    change max_seq_length param

--- src/finetuning/te/run_te.sh
+++ src/finetuning/te/run_te.sh
@@ -11,12 +11,12 @@ python $SCRIPT_DIR/run_te.py \
   --per_device_train_batch_size 4 \
   --gradient_accumulation_steps 2 \
   --num_train_epochs 10 \
-  --max_seq_length 512 \
+  --max_seq_length 128 \
   --load_best_model_at_end \
   --metric_for_best_model "accuracy" \
   --evaluation_strategy "epoch" \
   --save_strategy "epoch" \
   --seed 1 \
-  --logging_dir "$SCRIPT_DIR/roberta-base-ca-cased-te-fix/tb" \
-  --output_dir "$SCRIPT_DIR/roberta-base-ca-cased-te-fix"
+  --logging_dir "$SCRIPT_DIR/roberta-base-ca-cased-te/tb" \
+  --output_dir "$SCRIPT_DIR/roberta-base-ca-cased-te"
 
